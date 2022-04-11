1e5af8224da9e3c8389e8c4169517aa07b863cb4
Zadistic <nvalenzuela20@alumnos.utalca.cl>
2022-04-11 13:12:49 -0400

    ordenar codigo y cambiar nombre variables

--- test_week2.py
+++ test_week2.py
@@ -2,95 +2,98 @@ from audioop import mul
 from sqlite3 import Row
 from tkinter import*
 
-def press(num): #función que acepta los números a través de teclado.
+expresion = ""
 
-  global operador
-  operador = operador + str(num)
-  entradaTexto.set(operador)  #cambiar nombre variable
-  
+def ingresar(num):
+
+	global expresion
+
+	expresion = expresion + str(num)
+	ecuacion.set(expresion)
+
+
 def resolver():
 
-  try:
+	try:
 
-    global operador
-    final = str(eval(operador))
-    entradaTexto.set(final)
-    salida = ""
-  
-  except:
+		global expresion
 
-    entradaTexto.set(" error ")
-    salida = ""
-  
-def clear():
+		resultado = str(eval(expresion))
+		ecuacion.set(resultado)
+		expresion = ""
 
-    global operador
-    operador=("")
-    entradaTexto.set("0")
-  
-interfaz = Tk()
-interfaz.configure(bg="grey")
-interfaz.title("CALCULEITOR")
-interfaz.geometry("355x450")
+	except:
 
-entradaTexto = StringVar()
+		ecuacion.set(" error ")
+		expresion = ""
 
-operador=""
+def limpiar():
 
-salida = Entry(interfaz, font=("arial", 14, "bold"), width=19, textvariable=entradaTexto, bd=20, insertwidth=4,bg="white")
-salida.grid(columnspan=3, padx = 10, pady=20, ipadx=40, ipady=50, row= 0, column=0)
+	global expresion
+	expresion = ""
+	ecuacion.set("")
 
-ancho=7
-largo=1
+if __name__ == "__main__":
 
-case7 = Button(interfaz, text = " 7 ", width = ancho, height = largo, command=lambda: press(7))
-case7.grid(row=2, column=0)
+	interfaz = Tk()
+	interfaz.configure(background="grey75")
+	interfaz.title("Calculadora")
+	interfaz.geometry("373x450")
 
-case8 = Button(interfaz, text = " 8 ", width = ancho, height = largo, command=lambda: press(8))
-case8.grid(row=2, column=1)
+	ecuacion = StringVar()
 
-case9 = Button(interfaz, text=" 9 ", width=ancho, height=largo, command=lambda: press(9))
-case9.grid(row=2, column=2)
+	mostrar = Entry(interfaz, font=("arial", 14, "bold"), width=19, textvariable=ecuacion, bd=20, insertwidth=4,bg="white")
+	mostrar.grid(columnspan=4, padx = 20, pady=20, ipadx=50, ipady=80, row= 0, column=0)
 
-case4 = Button(interfaz, text=" 4 ", width=ancho, height=largo, command=lambda: press(4))
-case4.grid(row=3, column=0)
+	case1 = Button(interfaz, text=" 1 ", fg="white", bg="grey39", command=lambda: ingresar(1), height=1, width=7)
+	case1.grid(row=2, column=0)
 
-case5 = Button(interfaz, text=" 5 ", width=ancho, height=largo, command=lambda: press(5))
-case5.grid(row=3, column=1)
+	case2 = Button(interfaz, text=" 2 ", fg="white", bg="grey39", command=lambda: ingresar(2), height=1, width=7)
+	case2.grid(row=2, column=1)
 
-case6 = Button(interfaz, text=" 6 ", width=ancho, height=largo, command=lambda: press(6))
-case6.grid(row=3, column=2)
+	case3 = Button(interfaz, text=" 3 ", fg="white", bg="grey39", command=lambda: ingresar(3), height=1, width=7)
+	case3.grid(row=2, column=2)
 
-case1 = Button(interfaz, text=" 1 ", width=ancho, height=largo, command=lambda: press(1))
-case1.grid(row=4, column=0)
+	case4 = Button(interfaz, text=" 4 ", fg="white", bg="grey39", command=lambda: ingresar(4), height=1, width=7)
+	case4.grid(row=3, column=0)
 
-case2 = Button(interfaz, text=" 2 ", width=ancho, height=largo, command=lambda: press(2))
-case2.grid(row=4, column=1)
+	case5 = Button(interfaz, text=" 5 ", fg="white", bg="grey39", command=lambda: ingresar(5), height=1, width=7)
+	case5.grid(row=3, column=1)
 
-case3 = Button(interfaz, text=" 3 ", width=ancho, height=largo, command=lambda: press(3))
-case3.grid(row=4, column=2)
+	case6 = Button(interfaz, text=" 6 ", fg="white", bg="grey39", command=lambda: ingresar(6), height=1, width=7)
+	case6.grid(row=3, column=2)
 
-case0 = Button(interfaz, text=" 0 ", width=ancho, height=largo, command=lambda: press(0))
-case0.grid(row=5, column=1)
+	case7 = Button(interfaz, text=" 7 ", fg="white", bg="grey39", command=lambda: ingresar(7), height=1, width=7)
+	case7.grid(row=4, column=0)
 
-suma = Button(interfaz,text=" + ", width=ancho, height=largo, command=lambda: press("+"))
-suma.grid(row=5, column=2)
+	case8 = Button(interfaz, text=" 8 ", fg="white", bg="grey39", command=lambda: ingresar(8), height=1, width=7)
+	case8.grid(row=4, column=1)
 
-resta = Button(interfaz,text=" — ", width=ancho, height=largo, command=lambda: press("-"))
-resta.grid(row=5, column=0)
+	case9 = Button(interfaz, text=" 9 ", fg="white", bg="grey39", command=lambda: ingresar(9), height=1, width=7)
+	case9.grid(row=4, column=2)
 
-multip = Button(interfaz, text=" x ", width=ancho, height=largo, command=lambda: press("x"))
-multip.grid(row=6, column=0)
+	case0 = Button(interfaz, text=" 0 ", fg="white", bg="grey39", command=lambda: ingresar(0), height=1, width=7)
+	case0.grid(row=5, column=0)
 
-division = Button(interfaz, text=" / ", width=ancho, height=largo, command=lambda: press("/"))
-division.grid(row=6, column=1)
+	suma = Button(interfaz, text=" + ", fg="white", bg="grey39", command=lambda: ingresar("+"), height=1, width=7)
+	suma.grid(row=2, column=3)
 
-borrar = Button(interfaz, text=" C ", width=ancho, height=largo, command=clear)
-borrar.grid(row=6, column=2)
+	resta = Button(interfaz, text=" - ", fg="white", bg="grey39", command=lambda: ingresar("-"), height=1, width=7)
+	resta.grid(row=3, column=3)
 
-resol = Button(interfaz, text=" RESOLVER ", width=7, height=1, command=resolver, pady=10)
-resol.grid(row=7, column=0)
+	multip = Button(interfaz, text=" * ", fg="white", bg="grey39", command=lambda: ingresar("*"), height=1, width=7)
+	multip.grid(row=4, column=3)
 
-clear()
+	division = Button(interfaz, text=" / ", fg="white", bg="grey39", command=lambda: ingresar("/"), height=1, width=7)
+	division.grid(row=5, column=3)
 
-interfaz.mainloop()
+	resol = Button(interfaz, text=" = ", fg="white", bg="grey39", command=resolver, height=1, width=7)
+	resol.grid(row=6, column=3)
+
+	limpiar = Button(interfaz, text=" C ", fg="white", bg="maroon", command=limpiar, height=1, width=7)
+	limpiar.grid(row=5, column="2")
+
+	decimal= Button(interfaz, text=".", fg="white", bg="grey39", command=lambda: ingresar("."), height=1, width=7)
+	decimal.grid(row=5, column=1)
+	
+	interfaz.mainloop()
